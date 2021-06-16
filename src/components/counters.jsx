dbf92dc555113ab75f754803438e960ad0f07244
Adam Steinberger <adam@akmaz.io>
2021-06-16 07:58:38 -0400

    lesson 16 - bubble up events

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,13 +11,20 @@ class Counters extends Component {
     ],
   };
 
+  handleDelete = () => {
+    console.log("Event Handler Called");
+  };
+
   render() {
     return (
       <div>
         {this.state.counters.map((counter) => (
-          <Counter key={counter.id} value={counter.value} id={counter.id}>
-            <h4>Counter #{counter.id}</h4>
-          </Counter>
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            value={counter.value}
+            id={counter.id}
+          />
         ))}
       </div>
     );
